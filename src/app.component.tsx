bd579195f5f4dde658152809e32b2bb4e57ff935
Manish Kumar <87manishkumar@gmail.com>
2023-10-27 22:29:17 +0530

    commented static feedbacks

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -19,29 +19,29 @@ interface IFeedback {
   status: boolean;
 }
 
-const FEEDBACKS: Array<IFeedback> = [
-  {
-    user: "Rev. Shikha Ji",
-    content: "I love visiting Kids Exhibition. I learn a lot about the missions teachings from the models displayed in the kids exhibition.",
-    city: "Chandigarh",
-    status: true,
-    avatar: null
-  },
-  {
-    user: "Rev. Mandeep Ji",
-    content: "I enjoyed and learned a lot from Kids Exhibition. I like the whole exhibition a lot but my favourite is story tellling.",
-    city: "Gurgaon",
-    status: true,
-    avatar: null
-  },
-  {
-    user: "Rev. Yogesh Ji",
-    content: "I really enjoyed visiting the Kids Exhibition. Visiting kids exhibition is always a blissful experience. Models displayed are very thoughtful and connect us with the teachings of Satguru Mata Ji. May Satugru Mata Ji bless the the whole Children Exhibition team with all the blessings.",
-    city: "Panchkula",
-    status: true,
-    avatar: null
-  },
-];
+// const FEEDBACKS: Array<IFeedback> = [
+//   {
+//     user: "Rev. Shikha Ji",
+//     content: "I love visiting Kids Exhibition. I learn a lot about the missions teachings from the models displayed in the kids exhibition.",
+//     city: "Chandigarh",
+//     status: true,
+//     avatar: null
+//   },
+//   {
+//     user: "Rev. Mandeep Ji",
+//     content: "I enjoyed and learned a lot from Kids Exhibition. I like the whole exhibition a lot but my favourite is story tellling.",
+//     city: "Gurgaon",
+//     status: true,
+//     avatar: null
+//   },
+//   {
+//     user: "Rev. Yogesh Ji",
+//     content: "I really enjoyed visiting the Kids Exhibition. Visiting kids exhibition is always a blissful experience. Models displayed are very thoughtful and connect us with the teachings of Satguru Mata Ji. May Satugru Mata Ji bless the the whole Children Exhibition team with all the blessings.",
+//     city: "Panchkula",
+//     status: true,
+//     avatar: null
+//   },
+// ];
 
 function App() {
   const [slides, setSlides] = React.useState<IFeedback[]>([]);
